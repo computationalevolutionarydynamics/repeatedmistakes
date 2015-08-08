d7d24bfa54eda393106afc9c555caf656e2e010c
Nikolas M Skoufis <n.skoufis@gmail.com>
2015-08-08 16:20:33 +1000

    Change to nose testing for generator support
    Since we want to automatically grab all of the strategies from the strategies module and run them through the same set of tests, we need a way to automatically generate tests. Unittest wasn't allowing me to do this and we getting really messy and ugly.
    
    Nose makes this easy by allowing you to define tests that yield other functions which contain testing logic.

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -1,5 +1,6 @@
 from hypothesis import given
 from hypothesis.strategies import text
+import nose
 
 from repeatedmistakes import strategies
 
@@ -7,19 +8,24 @@ from repeatedmistakes import strategies
 Test all of the strategies in the strategies module to make sure that they only return a C (cooperate) or D (defect)
 regardless of their input. We do this by iterating over all of the callables in the strategies module.
 """
+# Grab all callables from the strategies module and make list of them
+strategy_list = [f for _, f in strategies.__dict__.items() if callable(f)]
 
-# Get all of the callables in the module, so that we can test them for compliance
-strategy_list = [f for _, f in strategies.__dict__.iteritems() if callable(f)]
+def check_returnsCsAndds(s, strategy):
+    """Make sure the function returns either a c or a d"""
+    assert strategy(s) in ['c', 'd']
 
-# Iterate over the strategies
-for strategy in strategy_list:
+@given(text(alphabet=['c', 'd']))
+def test_strategy_passHistoryTextContainingCsAndDs_returnsCsAndDs(s):
+    """Ensure that when a history string is passed, all we get back is either a c or a d"""
+    for strategy in strategy_list:
+        yield check_returnsCsAndds, s, strategy
 
-    @given(text(alphabet=['c', 'd']))
-    def test_strategy_passHistoryTextContainingCsAndDs_returnsCsAndDs(s):
-        """Ensure that when a history string is passed, all we get back is either a c or a d"""
-        assert strategy(s) in ['c', 'd']
+@given(text(alphabet=['C', 'D']))
+def test_strategy_passHistoryTextContainingUpperCase_returnsCsAndDs(s):
+    """Ensure that when a history string is passed with uppercase history, all we get back is either a c or a d"""
+    for strategy in strategy_list:
+        yield check_returnsCsAndds, s, strategy
 
-    @given(text(alphabet=['C', 'D']))
-    def test_strategy_passHistoryTextContainingUpperCase_returnsCsAndDs(s):
-        """Ensure that when a history string is passed with uppercase history, all we get back is either a c or a d"""
-        assert strategy(s) in ['c', 'd']
+if __name__ == '__main__':
+    nose.main()
