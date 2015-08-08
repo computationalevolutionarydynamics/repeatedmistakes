4b74f340cce8d80dd282c25d83ba69034d3e8ebe
Nikolas M Skoufis <n.skoufis@gmail.com>
2015-08-08 14:50:22 +1000

    Add a strategies module containing some basic strategies
    This module will contain the strategy functions that will model particular strategies in the iterated prisoner's dilemma.
    
    At the moment we have tit_for_tat, allc and alld

---
+++ repeatedmistakes/strategies.py
@@ -0,0 +1,74 @@
+"""
+Contains functions that model strategies for playing the iterated prisoner's dilemma
+
+Each function should take as input a string consisting of c's and d's that represents the history of the opponent's
+moves in a game of the iterated prisoner's dilemma, and return either a c or a d corresponding to that strategy's next
+move. Any input not of the form of string of cs or ds should raise a ValueError
+
+This module should not contain any non-strategy top-level callables, because the tests involve iterating over all of
+the callables and testing against them.
+"""
+from repeatedmistakes.strategyutils import valid_history
+
+def tit_for_tat(history):
+    """
+    The tit_for_tat strategy cooperates on the first round, and then plays whatever the opponent played last.
+
+    Args:
+        history (string): A string consisting of cs or ds (case insensitive) that represents the opponents moves
+
+    Returns:
+        A c or a d
+
+    Raises:
+        ValueError: If a character in the input is not recognised
+    """
+    # Make sure the history is a valid history string
+    if not valid_history(history):
+        raise ValueError('History string must consist of only cs and ds')
+
+    # If the history is empty, cooperate
+    if history == '':
+        return 'c'
+    else:
+        # Copy the opponent's last move
+        last_move = history[-1]
+        return last_move.lower()
+
+def allc(history):
+    """
+    The allc strategy always cooperates regardless of history.
+
+    Args:
+        history (string): A string consisting of cs or ds (case insensitive) that represents the opponents moves
+
+    Returns:
+        A c
+
+    Raises:
+        ValueError: If a character in the input is not recognised
+    """
+    # Make sure the history is a valid history string
+    if not valid_history(history):
+        raise ValueError('History string must consist of only cs and ds')
+
+    return 'c'
+
+def alld(history):
+    """
+    The alld strategy always cooperates regardless of history.
+
+    Args:
+        history (string): A string consisting of cs or ds (case insensitive) that represents the opponents moves
+
+    Returns:
+        A d
+
+    Raises:
+        ValueError: If a character in the input is not recognised
+    """
+    # Make sure the history is a valid history string
+    if not valid_history(history):
+        raise ValueError('History string must consist of only cs and ds')
+
+    return 'd'
