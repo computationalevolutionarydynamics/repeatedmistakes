e9c239fc4a6d651247d04eb3c1b2592bcb6bbca6
Nikolas M Skoufis <n.skoufis@gmail.com>
2015-08-09 19:22:13 +1000

    Add tests for specific strategies
    This adds the following tests:
    * Check that allc only returns c
    * Check that alld only returns d
    * Check that tit for tat returns a c if passed an empty history
    
    This patch also changes the way that the histories are generated by Hypothesis. This syntax is clearer, and I think the list syntax was actually causing unexpected behaviour.

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -1,5 +1,5 @@
 from hypothesis import given
-from hypothesis.strategies import text
+from hypothesis.strategies import text, just
 import nose
 
 from repeatedmistakes import strategies
@@ -15,17 +15,31 @@ def check_returnsCsAndds(s, strategy):
     """Make sure the function returns either a c or a d"""
     assert strategy(s) in ['c', 'd']
 
-@given(text(alphabet=['c', 'd']))
+@given(text(alphabet='cd'))
 def test_strategy_passHistoryTextContainingCsAndDs_returnsCsAndDs(s):
     """Ensure that when a history string is passed, all we get back is either a c or a d"""
     for strategy in strategy_list:
         yield check_returnsCsAndds, s, strategy
 
-@given(text(alphabet=['C', 'D']))
+@given(text(alphabet='CD'))
 def test_strategy_passHistoryTextContainingUpperCase_returnsCsAndDs(s):
     """Ensure that when a history string is passed with uppercase history, all we get back is either a c or a d"""
     for strategy in strategy_list:
         yield check_returnsCsAndds, s, strategy
+
+# Test individual strategies to make sure that they return the correct value as per their desired behaviour
+@given(text(alphabet='cd'))
+def test_allc_passAnyHistory_returnsC(s):
+    assert strategies.allc(s) == 'c'
+
+@given(text(alphabet='cd'))
+def test_alld_passAnyHistory_returnsD(s):
+    assert strategies.alld(s) == 'd'
+
+@given(just(''))
+def test_titForTat_passEmptyHistory_strategyCooperates(s):
+    assert strategies.tit_for_tat(s) == 'c'
+
 
 if __name__ == '__main__':
     nose.main()
