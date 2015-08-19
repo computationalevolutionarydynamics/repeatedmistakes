a6d77faffa7ca53a3439042169918e8ac41381cf
Nikolas M Skoufis <n.skoufis@gmail.com>
2015-08-19 18:36:46 +1000

    Update method for simulating normalised payoff
    This updates the method simulate_expected_payoff in two ways.
    
    Firstly, it updates it to simulate_normalised_payoff .ie we normalise the payoff we are calculating by 1-continuation_probability.
    
    Secondly, it updates it to use the new class based strategies.
    
    Finally, it now simultaneously calculates the normalised payoffs for both strategies.

--- repeatedmistakes/simulations.py
+++ repeatedmistakes/simulations.py
@@ -4,56 +4,67 @@ Contains functions used to simulate different scenarios involving the iterated p
 from numpy.random import RandomState
 
 
-def simulate_expected_payoff(player, opponent, payoff_matrix, continuation_probability, trials=1000, seed=1234):
+def simulate_normalised_payoff(strategy_one, strategy_two, payoff_matrix, continuation_probability, trials=1000, seed=1234):
     """
-    Calculate the expected payoff for a particular strategy and opponent in the iterated prisoner's dilemma
+    Calculate the normalised payoff for each strategy in the iterated prisoner's dilemma
 
-    This method calculates the expected payoff for a particular strategy against an opponent strategy with a given
-    payoff structure and continuatione probability in the iterated prisoners dilemma using a monte-carlo method to
-    simulate games of random length.
+    This method calculates the normalised payoff for two strategies in the iterated prisoner's dilemma using a monte
+    carlo method. The prisoner's dilemma is played for a random number of rounds (as determined by the continuation
+    probability) for some given number of trials, and the normalised payoff of each strategy is then computed.
 
     Args:
-         player (callable): A callable that represents the player for which the payoff is to be calculated. This callable
-            should take as an argument a history of moves (in the form of a string of cs and ds) and return either a
-            c or a d representing cooperation or defection in the iterated prisoner's dilemma.
-        opponent (callable): A callable that represents the opponent of the player. The opponent should have similar
-            behaviour to the player.
-        payoff_matrix (pandas.DataFrame): A pandas dataframe that holds the payoff matrix. It is expected that columns
-            and rows are labelled c and d, with the player along the columns and the opponent along the rows
-        continuation_probability (float): The probability that another game is played after each round.
-        trials (int): The number of games to simulate.
+        strategy_one (Strategy): The strategy to be tested
+        strategy_two (Strategy): The other strategy
+        payoff_matrix (PayoffMatrix): A payoff matrix representing the rewards for each set of actions
+        continuation_probability (float): The probability of continuing the game
+        trials (int): The number of games to simulate in order to calculate the normalised payoff
+        seed (int): The seed for the PRNG
 
     Returns:
-        expected_payoff (float): The expected payoff for the player
+        normalised_payoff (dict): A dictionary where the keys are the strategies and the values are the normalised
+            payoff.
     """
     # Create an PRNG instance and seed it
     random_instance = RandomState(seed)
-    total_payoff = 0
+
+    normalised_payoff = {strategy_one: 0, strategy_two: 0}
+
+    # Create the strategy objects, taking the characterset from the payoff_matrix
+    player_one = strategy_one(C=payoff_matrix.C, D=payoff_matrix.D)
+    player_two = strategy_two(C=payoff_matrix.C, D=payoff_matrix.D)
 
     # We want trials number of games
     for _ in range(trials):
         continue_game = 0.
+        player_one_total_payoff  = 0.
+        player_two_total_payoff  = 0.
 
-        # Reset the histories of each player
-        player_history = ''
-        opponent_history = ''
+        # Reset each player
+        player_one.reset()
+        player_two.reset()
         # We want to repeat until we pick a random number that's greater than the continuation probability
         while continue_game < continuation_probability:
             # Figure out what move each player plays
-            player_move = player(opponent_history)
-            opponent_move = opponent(player_history)
+            move_one = player_one(player_two.history)
+            move_two = player_two(player_one.history)
             # Add these moves to each player's history
-            player_history += player_move
-            opponent_history += opponent_move
-            # Get the payoff for this set of moves
-            payoff = payoff_matrix[player_move][opponent_move]
+            player_one.history += move_one
+            player_two.history += move_two
+            # Get the payoff for each player for this set of moves
+            player_one_payoff, player_two_payoff = payoff_matrix.payoff(player_one=move_one, player_two=move_two)
             # Add this to the expected payoff
-            total_payoff += payoff
-
+            player_one_total_payoff += player_one_payoff
+            player_two_total_payoff += player_two_payoff
             # Choose a random float between zero and one. If this is greater than the continuation probability, the game
             # stops
             continue_game = random_instance.random_sample()
 
-    # Divide the total payoff by the number of trials to give the expected payoff
-    expected_payoff = total_payoff / trials
-    return expected_payoff
+        # Multiply each payoff by (1-continuation_probability) to normalise it, then add it to the dict value
+        normalised_payoff[player_one] += (1 - continuation_probability) * player_one_total_payoff
+        normalised_payoff[player_two] += (1 - continuation_probability) * player_two_total_payoff
+
+    # Divide the payoffs by the total number of trials and return the final answer
+    normalised_payoff[player_one] /= trials
+    normalised_payoff[player_two] /= trials
+
+    return normalised_payoff
