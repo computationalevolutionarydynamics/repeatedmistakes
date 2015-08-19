1098c20727f8af9eaefc2723684a529086431b86
Nikolas M Skoufis <n.skoufis@gmail.com>
2015-08-19 19:46:04 +1000

    Update normalised payoff calculation for classes
    This updates the method that calculates the normalised payoff using an infinite sum to use the new strategy class interface.

--- repeatedmistakes/calculations.py
+++ repeatedmistakes/calculations.py
@@ -4,9 +4,9 @@ computations.
 """
 import numpy as np
 
-def calculate_normalised_payoff(player, opponent, payoff_matrix, continuation_probability, epsilon):
+def calculate_normalised_payoff(strategy_one, strategy_two, payoff_matrix, continuation_probability, epsilon):
     """
-    Calculate the normalised payoff for a particular strategy and opponent in the iterated prisoner's dilemma
+    Calculate the normalised payoff for strategies in the iterated prisoner's dilemma
 
     This method is used to calculate the normalised value of the payoff when two strategies play each other in the
     iterated prisoner's dilemma with a given payoff structure and continuation probability.
@@ -14,18 +14,14 @@ def calculate_normalised_payoff(player, opponent, payoff_matrix, continuation_pr
     is truncated and the result is returned.
 
     Args:
-        player (callable): A callable that represents the player for which the payoff is to be calculated. This callable
-            should take as an argument a history of moves (in the form of a string of cs and ds) and return either a
-            c or a d representing cooperation or defection in the iterated prisoner's dilemma.
-        opponent (callable): A callable that represents the opponent of the player. The opponent should have similar
-            behaviour to the player.
-        payoff_matrix (pandas.DataFrame): A pandas dataframe that holds the payoff matrix. It is expected that columns
-            and rows are labelled c and d, with the player along the columns and the opponent along the rows
+        strategy_one (Strategy): The first strategy in the game
+        strategy_two (Strategy): The other strategy in the game
+        payoff_matrix (PayoffMatrix): An object that gives the payoff for each player given certain actions
         continuation_probability (float): The probability that another game is played after each round.
         epsilon (float): The minimum size of terms in the sum before the sum is truncated and the result is returned
 
     Returns:
-        normalised_payoff (float): The normalised payoff for the player
+        normalised_payoff (dict): A dict where the keys are the strategies and the values are the normalised payoffs
 
     Raises:
         ValueError: If the continuation probability is greater than or equal to 1, as this would mean that the sum
@@ -34,27 +30,33 @@ def calculate_normalised_payoff(player, opponent, payoff_matrix, continuation_pr
     if continuation_probability >= 1:
         raise ValueError('Continuation probability must be less than 1 for the sum to converge')
 
-    normalised_payoff = 0
-    player_history = ''
-    opponent_history = ''
+    normalised_payoff = {strategy_one: 0, strategy_two: 0}
 
-    term = np.Infinity
+    # Create the player objects using the characterset from the payoff matrix
+    player_one = strategy_one(C=payoff_matrix.C, D=payoff_matrix.D)
+    player_two = strategy_two(C=payoff_matrix.C, D=payoff_matrix.D)
+
+    player_one_term = np.Infinity
+    player_two_term = np.Infinity
     rounds = 0
-    while term > epsilon:
+    while player_one_term > epsilon or player_two_term > epsilon:
         rounds += 1
         # Compute the moves that each strategy makes
-        player_move = player(opponent_history)
-        opponent_move = opponent(player_history)
+        move_one = player_one.next_move(player_two.history)
+        move_two = player_two.next_move(player_one.history)
         # Add these moves to each history
-        player_history += player_move
-        opponent_history += opponent_move
+        player_one.history += move_one
+        player_two.history += move_two
         # Get the payoff resulting from the last game
-        payoff = payoff_matrix[player_move][opponent_move]
+        player_one_payoff, player_two_payoff = payoff_matrix(player_one=move_one, player_two=move_two)
         # Compute the term from the sum
-        term = (continuation_probability ** rounds) * payoff
-        normalised_payoff += term
+        player_one_term = (continuation_probability ** rounds) * player_one_payoff
+        player_two_term = (continuation_probability ** rounds) * player_two_payoff
+        normalised_payoff[strategy_one] += player_one_term
+        normalised_payoff[strategy_two] += player_two_term
 
     # Multiply by (1 - continuation_probability) to normalise the value
-    normalised_payoff = (1 - continuation_probability) * normalised_payoff
+    normalised_payoff[player_one] *= (1 - continuation_probability)
+    normalised_payoff[player_two] *= (1 - continuation_probability)
 
     return normalised_payoff
