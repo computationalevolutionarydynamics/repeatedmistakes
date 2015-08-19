25c0d5cd5a48cef80bd54d73fe675262483c75c7
Nikolas M Skoufis <n.skoufis@gmail.com>
2015-08-19 16:47:35 +1000

    Add test for length of returned history
    Add a test that ensures that the histories returned by RepeatedGame.simulate match the number of rounds.

---
+++ repeatedmistakes/tests/test_repeatedgame.py
@@ -0,0 +1,24 @@
+"""
+Tests for the repeated game class
+"""
+from repeatedmistakes import strategies
+from repeatedmistakes.repeatedgame import RepeatedGamne
+
+from hypothesis import given
+from hypothesis.strategies import sampled_from, integers
+import nose
+
+# We want to test that given any combination of strategies and any number of rounds, the result we get back from the
+# simulate function has the correct number of elements in each history
+@given(strat1=sampled_from(strategies.strategy_list), strat2=sampled_from(strategies.strategy_list),
+       rounds=integers(min_value=0))
+
+def test_repeatedGameSimulate_passStrategiesAndNumberOfRounds_ReturnedHistoriesMatchInLength(strat1, strat2, rounds):
+    """Test that any simulating any number of rounds with any strategies gives results of the correct length"""
+    game = RepeatedGamne(strat1, strat2)
+    results = game.simulate(rounds)
+    assert len(results[strat1]) == rounds
+    assert len(results[strat2]) == rounds
+
+if __name__ == '__main__':
+    nose.main()
