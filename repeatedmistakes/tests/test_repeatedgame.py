a2b20a32817da065ad7a1e9e5c5b741fa78ff303
Nikolas M Skoufis <n.skoufis@gmail.com>
2015-08-28 01:16:45 +1000

    Reduce the maxmimum length to test
    Since we're using integer to figure out how many rounds to simulate, my theory is that it was simulating absurd numbers or rounds like 10000000+. I've limited it to 1000 for time's sake.

--- repeatedmistakes/tests/test_repeatedgame.py
+++ repeatedmistakes/tests/test_repeatedgame.py
@@ -11,7 +11,7 @@ import nose
 # We want to test that given any combination of strategies and any number of rounds, the result we get back from the
 # simulate function has the correct number of elements in each history
 @given(strat1=sampled_from(strategies.strategy_list), strat2=sampled_from(strategies.strategy_list),
-            rounds=integers(min_value=0))
+            rounds=integers(min_value=0, max_value=1000))
 def test_repeatedGameSimulate_passStrategiesAndNumberOfRounds_ReturnedHistoriesMatchInLength(strat1, strat2, rounds):
     """Test that any simulating any number of rounds with any strategies gives results of the correct length"""
     game = RepeatedGame(strat1, strat2)
