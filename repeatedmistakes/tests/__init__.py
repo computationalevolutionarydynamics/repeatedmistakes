7e24ff9c0fccb12ed28438406797734eab5ea110
Nikolas M Skoufis <n.skoufis@gmail.com>
2015-08-28 01:00:51 +1000

    Increase the default number of test cases

--- repeatedmistakes/tests/__init__.py
+++ repeatedmistakes/tests/__init__.py
@@ -5,3 +5,5 @@ def setup():
     Settings.default.timeout = 0
     # We also want to derandomize the testing, because sometimes it times out and sometimes it doesn't.
     Settings.default.derandomize = True
+    # And we want a larger number of examples
+    Settings.default.max_examples = 750
