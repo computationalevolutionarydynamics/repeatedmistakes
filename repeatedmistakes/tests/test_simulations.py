a50e481536bd78c08bfcda21a7db2d14fe669e1f
Nikolas M Skoufis <n.skoufis@gmail.com>
2015-08-27 17:27:54 +1000

    Add tests for simulations method
    This adds a test which compares the values computed by simulating the repeated game and then calculating the normalised payoff against calculating the normalised payoff from a sum.

---
+++ repeatedmistakes/tests/test_simulations.py
@@ -0,0 +1,45 @@
+from repeatedmistakes.simulations import simulate_normalised_payoff
+from repeatedmistakes.calculations import calculate_normalised_payoff
+from repeatedmistakes.tests.test_calculations import strategy_combinations
+from repeatedmistakes.repeatedgame import PrisonersDilemmaPayoff
+
+from hypothesis import given
+from hypothesis.strategies import floats, tuples
+import nose
+
+"""
+Here we want to test the simulations against the numerical calculations. We will reuse the sets of strategy pairs from
+the tests for the calculations
+"""
+# The global epsilon for trunacting the calculation sum
+EPSILON = 0.0001
+# The tolerance allowed between the simulations and the calculations
+TOLERANCE = 0.01
+# The number of trials of the simulations to run
+TRIALS = 10000
+
+small_float = floats(min_value=0, max_value=10)
+delta = floats(min_value=0.01, max_value = 0.99)
+@given(payoff_values=tuples(small_float, small_float, small_float, small_float), delta=delta)
+def test_simulations_passAnyDeltaAndPayoffMatrix_simulationsMatchCalculations(payoff_values, delta):
+    """This tests that the results returned by the simulations match the results of the calculations."""
+    # Construct the payoff matrix
+    payoff_matrix = PrisonersDilemmaPayoff(P = payoff_values[0], R = payoff_values[1],
+                                           S = payoff_values[2], T = payoff_values[3])
+    # We need to loop over the strategy combinations
+    for combo in strategy_combinations:
+        # Pull out each strategy
+        strategy_one = combo[0]
+        strategy_two = combo[1]
+        # Get the result from the calcs
+        calculation_result, _ = calculate_normalised_payoff(strategy_one, strategy_two, payoff_matrix, delta, EPSILON)
+        # Get the result from the sims
+        simulation_result, _ = simulate_normalised_payoff(strategy_one, strategy_two, payoff_matrix, delta, TRIALS)
+        # Compare them
+        if abs(simulation_result) > TOLERANCE:
+            assert abs(simulation_result - calculation_result) / abs(calculation_result) <= TOLERANCE
+        else:
+            assert abs(simulation_result - calculation_result) <= TOLERANCE
+
+if __name__ == '__main__':
+    nose.main()
