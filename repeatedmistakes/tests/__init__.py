bdeea0cac6c25884f8c19db101b6be7d10936fbb
Nikolas M Skoufis <n.skoufis@gmail.com>
2015-08-27 18:56:20 +1000

    De-randomize hypothesis execution
    Since hypothesis seems to sometimes timout when searching for tests cases and sometimes it doesnt', we're going to try de-randomising it's execution.

--- repeatedmistakes/tests/__init__.py
+++ repeatedmistakes/tests/__init__.py
@@ -3,3 +3,5 @@ from hypothesis import Settings
 def setup():
     # We want to set the default hypothesis settings object to have no timeout
     Settings.default.timeout = 0
+    # We also want to derandomize the testing, because sometimes it times out and sometimes it doesn't.
+    Settings.default.derandomize = True
