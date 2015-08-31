5aab9ffe2a0fb9bd664c0e59009c3b9707da664c
Nikolas Skoufis <n.skoufis@gmail.com>
2015-08-31 15:29:55 +1000

    Maybe fixed comparison of Allc and Allc

--- repeatedmistakes/AllCAllCMistakes.py
+++ repeatedmistakes/AllCAllCMistakes.py
@@ -9,13 +9,22 @@ from repeatedmistakes.simulations import simulate_normalised_payoff
 
 def compute_values():
     payoff_matrix = PrisonersDilemmaPayoff()
-    delta = 0.8
-    mu = 0.1
-    sim = simulate_normalised_payoff(AllC, AllC, payoff_matrix, delta, mistake_probability=mu, trials=10000)
-    calc = 0.
-    for i in range(0, 100):
-        calc += (delta ** i) * ((mu * mu * payoff_matrix.DD) + ((mu * (1 - mu) * payoff_matrix.CD)) +
-                               ((mu * (1 - mu)) * payoff_matrix.DC) + ((1 - mu) * (1 - mu) * payoff_matrix.CC))
+    delta = 0.9
+    mu = 0.05
+    sim = simulate_normalised_payoff(AllC, AllC, payoff_matrix, delta, mistake_probability=mu, estimator_stdev=0.2)
+    calc = [0., 0.]
+    no_mistake = [0., 0.]
+    one_mistake = [0., 0.]
+    two_mistakes = [0., 0.]
+    for i in range(0, 10000):
+        for k in [0, 1]:
+            no_mistake[k] = ((1 - mu) ** 2) * (payoff_matrix.CC)[k]
+            two_mistakes[k] =  (mu ** 2) * (payoff_matrix.DD)[k]
+            one_mistake[k] = (mu * (1 - mu)) * ((payoff_matrix.CD)[k] + (payoff_matrix.DC)[k])
+            calc[k] += (delta ** i) * (no_mistake[k] + one_mistake[k] + two_mistakes[k])
+
+    calc[0] *= (1 - delta)
+    calc[1] *= (1 - delta)
 
     print("Calculated value = " + str(calc))
     print("Simulated value = " + str(sim))
