c2a686417e308cea1e425317d51e359f284fddfc
Nikolas M Skoufis <n.skoufis@gmail.com>
2015-09-05 01:10:52 +1000

    Remove files from the gitignore
    The files in the repo got kind of mangled. This should hopefully remove
    pycache, hypothesis and pycharm specific files from the repo, and they
    shouldn't be added again due to the gitignore.

--- repeatedmistakes/.hypothesis/eval_source/hypothesis_temporary_module_097730795b3b7594930b0bd849b9e61595180e53.py
+++
@@ -1,6 +0,0 @@
-from hypothesis.utils.conventions import not_set
-
-def accept(f):
-    def test_calculations_singleResultCombosGiven_ExpectedResultReturned(payoff_values=not_set, delta=not_set, combo=not_set):
-        return f(payoff_values, delta, combo)
-    return test_calculations_singleResultCombosGiven_ExpectedResultReturned
