05df193d17719e81f4852c04967494aa5fddf955
Nikolas Skoufis <n.skoufis@gmail.com>
2015-10-12 22:04:55 +1100

    Add extra options to batch
    Add stdout and stderr redirects, ncpus option

--- cluster/generate.sh
+++ cluster/generate.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Based on code found at https://stackoverflow.com/a/6215113/4042597
 
-# This file generates a job file based on the passed paramters, and it prints
+# This file generates a job file based on the passed parameters, and it prints
 # this to the standard output
 
 # Define parameters which are passed in.
@@ -14,6 +14,9 @@ cat << EOF
 #PBS -S /bin/sh
 #PBS -m bea
 #PBS -l mem=1000mb
+#PBS -o simulations_$delta_$gamma
+#PBS -e simulations_$delta_$gamma_error
+#PBS -l ncpus=8
 echo "Current working directory is `pwd`"
 echo "Starting run "$0" at: `date`"
 module load python/3.4.3
