b5d2e75ff61ee130d1976fc479c6738a98bf5096
Nikolas M Skoufis <n.skoufis@gmail.com>
2015-10-12 19:13:29 +1100

    Add generator script to create job files based on parameters
    This adds a script called generate.sh which will generate a jobfile to
    the standard output when pass some command line parameters. This will
    be used as part of another script which will create a bunch of jobfiles
    over a range or list of paramters.

---
+++ cluster/generate.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Based on code found at https://stackoverflow.com/a/6215113/4042597
+
+# This file generates a job file based on the passed paramters, and it prints
+# this to the standard output
+
+# Define parameters which are passed in.
+delta=$1    # The continuation probability
+gamma=$2    # The mistake probability
+
+# Define the template.
+cat << EOF
+#!/bin/sh
+#PBS -S /bin/sh
+#PBS -m bea
+#PBS -l mem=1000mb
+echo "Current working directory is `pwd`"
+echo "Starting run "$0" at: `date`"
+module load python/3.4.3
+python3 simulation.py $delta $gamma
+echo "Program "$0" finished with exit code $? at: `date`"
+EOF
